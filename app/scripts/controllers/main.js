197c85ea4a9dcfe9708289f5253bb7c83339bea7
Rob Szumski <rob@robszumski.com>
2013-09-10 15:04:22 -0700

    Add better relative timestamps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -145,3 +145,21 @@ angular.module('etcdApp').directive('ngKeydown', function() {
         });
     };
 });
+
+moment.lang('en', {
+    relativeTime : {
+        future: "Expires in %s",
+        past:   "Expired %s ago",
+        s:  "seconds",
+        m:  "a minute",
+        mm: "%d minutes",
+        h:  "an hour",
+        hh: "%d hours",
+        d:  "a day",
+        dd: "%d days",
+        M:  "a month",
+        MM: "%d months",
+        y:  "a year",
+        yy: "%d years"
+    }
+});
