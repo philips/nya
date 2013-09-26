bb930ce29d9cf032f3b527ae565a0f9a180b5315
Brandon Philips <brandon@ifup.co>
2013-09-26 14:01:44 -0700

    chore(*): cleanup some jshint stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,9 +5,9 @@ angular.module('etcdApp')
   //Allows CORS to work properly
   delete $http.defaults.headers.common['X-Requested-With'];
 
-  $scope.save = "etcd-save-hide";
-  $scope.preview = "etcd-preview-hide";
-  $scope.etcd_path = "/v1/keys/";
+  $scope.save = 'etcd-save-hide';
+  $scope.preview = 'etcd-preview-hide';
+  $scope.etcd_path = '/v1/keys/';
   $scope.enable_back = true;
   $scope.writingNew = false;
 
@@ -20,22 +20,22 @@ angular.module('etcdApp')
       //write to localstorage
       localStorage.setItem('etcd_path', $scope.etcd_path);
 
-      var current_path = $scope.etcd_path.split("/");
+      var current_path = $scope.etcd_path.split('/');
       var parent_path;
       //empty strings that used to be /
       current_path = current_path.filter(function(v){return v!==''});
       //remove last item
       current_path.pop();
       //reconstruct path
-      parent_path = current_path.join("/");
+      parent_path = current_path.join('/');
 
-      $scope.etcd_parent_path = "/" + parent_path + "/";
+      $scope.etcd_parent_path = '/' + parent_path + '/';
 
       //load data
       read();
 
       //disable back button if at root (/v1/keys/)
-      if($scope.etcd_path == "/v1/keys/") {
+      if($scope.etcd_path == '/v1/keys/') {
         $scope.enable_back = false;
       } else {
         $scope.enable_back = true;
@@ -47,21 +47,21 @@ angular.module('etcdApp')
   function read() {
     $http.get('http://localhost:4001' + $scope.etcd_path).success(function(data) {
       //hide any errors
-      $("#etcd-browse-error").hide();
+      $('#etcd-browse-error').hide();
       //swap this out with better logic later
       if(data.length) {
         $scope.list = data;
-        $scope.preview = "etcd-preview-hide";
+        $scope.preview = 'etcd-preview-hide';
       } else {
         $scope.single_value = data.value;
-        $scope.preview = "etcd-preview-reveal";
+        $scope.preview = 'etcd-preview-reveal';
         $http.get('http://localhost:4001' + $scope.etcd_parent_path).success(function(data) {
           $scope.list = data;
         });
       }
-        $scope.preview_message = "No key selected."
+        $scope.preview_message = 'No key selected.'
     }).error(function (data, status, headers, config) {
-      $scope.preview_message = "Key does not exist."
+      $scope.preview_message = 'Key does not exist.'
       $http.get('http://localhost:4001' + $scope.etcd_parent_path).success(function(data) {
         $scope.list = data;
       });
@@ -72,7 +72,7 @@ angular.module('etcdApp')
 
   //back button click
   $scope.back = function() {
-    var path = $scope.etcd_path.split("/");
+    var path = $scope.etcd_path.split('/');
     var path_length = path.length;
     var new_path;
     //empty strings that used to be /
@@ -80,13 +80,13 @@ angular.module('etcdApp')
     //remove last item
     path.pop();
     //reconstruct path
-    new_path = path.join("/");
+    new_path = path.join('/');
     //record new path if it doesn't back up too much
-    if(new_path != "v1") {
-      $scope.etcd_path = "/" + new_path + "/";
+    if(new_path != 'v1') {
+      $scope.etcd_path = '/' + new_path + '/';
     }
     $location.path($scope.etcd_path);
-        $scope.preview = "etcd-preview-hide";
+        $scope.preview = 'etcd-preview-hide';
         $scope.writingNew = false;
   }
 
@@ -95,21 +95,21 @@ angular.module('etcdApp')
   }
 
   $scope.show_save = function() {
-    $scope.save = "etcd-save-reveal";
+    $scope.save = 'etcd-save-reveal';
   }
 
   $scope.save_data = function() {
     //TODO: add loader
     $http({
       url: 'http://localhost:4001' + $scope.etcd_path,
-      method: "POST",
-      //data: "value=" + $scope.single_value,
+      method: 'POST',
+      //data: 'value=' + $scope.single_value,
       data: $.param({value: $scope.single_value}),
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     }).success(function (data, status, headers, config) {
       //TODO: remove loader
-      $scope.save = "etcd-save-hide";
-      $scope.preview = "etcd-preview-hide";
+      $scope.save = 'etcd-save-hide';
+      $scope.preview = 'etcd-preview-hide';
       $scope.back();
       $scope.writingNew = false;
     }).error(function (data, status, headers, config) {
@@ -123,43 +123,43 @@ angular.module('etcdApp')
     //TODO: add loader
     $http({
       url: 'http://localhost:4001' + $scope.etcd_path,
-    method: "DELETE",
+    method: 'DELETE',
     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     }).success(function (data, status, headers, config) {
       //TODO: remove loader
-      $scope.save = "etcd-save-hide";
-      $scope.preview = "etcd-preview-hide";
+      $scope.save = 'etcd-save-hide';
+      $scope.preview = 'etcd-preview-hide';
       $scope.back();
     }).error(function (data, status, headers, config) {
       //TODO: remove loader
       //show errors
-      $scope.showBrowseError("Error: Could not delete the key");
+      $scope.showBrowseError('Error: Could not delete the key');
     });
   }
 
   $scope.add = function() {
-    $scope.save = "etcd-save-reveal";
-    $scope.preview = "etcd-preview-reveal";
-    $scope.single_value = "";
-    $(".etcd-browser-path").find("input").focus();
+    $scope.save = 'etcd-save-reveal';
+    $scope.preview = 'etcd-preview-reveal';
+    $scope.single_value = '';
+    $('.etcd-browser-path').find('input').focus();
     $scope.writingNew = true;
   }
 
   $scope.showBrowseError = function(message) {
-    $("#etcd-browse-error").find(".etcd-popover-content").text("Error: " + message);
-    $("#etcd-browse-error").addClass("etcd-popover-right").show();
+    $('#etcd-browse-error').find('.etcd-popover-content').text('Error: ' + message);
+    $('#etcd-browse-error').addClass('etcd-popover-right').show();
   }
 
   $scope.showSaveError = function(message) {
-    $("#etcd-save-error").find(".etcd-popover-content").text("Error: " + message);
-    $("#etcd-save-error").addClass("etcd-popover-left").show();
+    $('#etcd-save-error').find('.etcd-popover-content').text('Error: ' + message);
+    $('#etcd-save-error').addClass('etcd-popover-left').show();
   }
 
   $scope.getHeight = function() {
     return $(window).height();
   };
   $scope.$watch($scope.getHeight, function(newValue, oldValue) {
-    $(".etcd-body").css("height", $scope.getHeight()-45);
+    $('.etcd-body').css('height', $scope.getHeight()-45);
   });
   window.onresize = function(){
     $scope.$apply();
@@ -169,7 +169,7 @@ angular.module('etcdApp')
 
 angular.module('etcdApp').directive('ngEnter', function() {
     return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
+        element.bind('keydown keypress', function(event) {
             if(event.which === 13) {
                 scope.$apply(function(){
                     scope.$eval(attrs.ngEnter);
@@ -185,8 +185,8 @@ angular.module('etcdApp').directive('highlight', ['$location', function(location
     return {
         restrict: 'A',
         link: function(scope, element, attrs, controller) {
-            if("#" + scope.etcd_path == attrs.href) {
-              element.parent().parent().addClass("etcd-selected")
+            if('#' + scope.etcd_path == attrs.href) {
+              element.parent().parent().addClass('etcd-selected')
             }
         }
     };
@@ -194,18 +194,18 @@ angular.module('etcdApp').directive('highlight', ['$location', function(location
 
 moment.lang('en', {
   relativeTime : {
-    future: "Expires in %s",
-    past:   "Expired %s ago",
-    s:  "seconds",
-    m:  "a minute",
-    mm: "%d minutes",
-    h:  "an hour",
-    hh: "%d hours",
-    d:  "a day",
-    dd: "%d days",
-    M:  "a month",
-    MM: "%d months",
-    y:  "a year",
-    yy: "%d years"
+    future: 'Expires in %s',
+    past:   'Expired %s ago',
+    s:  'seconds',
+    m:  'a minute',
+    mm: '%d minutes',
+    h:  'an hour',
+    hh: '%d hours',
+    d:  'a day',
+    dd: '%d days',
+    M:  'a month',
+    MM: '%d months',
+    y:  'a year',
+    yy: '%d years'
   }
 });
