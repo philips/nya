9909d70237cc040cbc4d858da182cd9b54d1ceee
Brandon Philips <brandon@ifup.co>
2013-09-26 13:30:39 -0700

    feat(stats): stats now come from the stats API
    instead of randomly generating the stats get them from the API.

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -3,109 +3,74 @@
 angular.module('etcd-stats')
   .controller('StatsCtrl', function ($scope, $http) {
 
-    $scope.graphContainer = "#latency";
-    $scope.graphVisibility = "etcd-graph-show";
+  $scope.graphContainer = "#latency";
+  $scope.graphVisibility = "etcd-graph-show";
+  $scope.tableVisibility = "etcd-table-hide";
+
+  //make requests
+  function read() {
+    $http.get('http://localhost:4001/v1/stats/leader').success(function(data) {
+      $scope.leaderStats = data;
+      $scope.followers = []
+      $.each(data.peers, function(index, value) {
+        value.name = index
+        $scope.followers.push(value)
+      });
+      drawGraph()
+    }).error(function (data, status, headers, config) {
+      $scope.showBrowseError(data.message);
+    });
+  }
+
+  function drawGraph () {
+    //hardcoded padding from chart json
+    var vert_padding = 30;
+    var hor_padding = 15;
+    //fetch width and height of graph area
+    var width = $($scope.graphContainer).width() - hor_padding;
+    var height = $($scope.graphContainer).height() - vert_padding;
+
+    // parse a spec and create a visualization view
+    function parse(spec) {
+      vg.parse.spec(spec, function(chart) {
+        chart({
+          el: $scope.graphContainer,
+          data: {
+            "stats": $scope.followers
+          }
+        }).width(width).height(height).update();
+      });
+    }
+    parse("../etcd-latency.json");
+  }
+
+  $scope.show_table = function() {
+    $scope.tableVisibility = "etcd-table-reveal";
+  }
+
+  $scope.show_graph = function() {
     $scope.tableVisibility = "etcd-table-hide";
+  }
 
-    /*{
-        "gets":12,
-        "sets":7,
-        "deletes":0,
-        "testAndSets":0
-        "testAndSets":0
-    }*/
-    /*var raw_json = '{
-        "followers": [
-            {
-                "hostname": "machine0",
-                "role": "leader",
-                "currentLatency":1.400807,
-                "averageLatency":1.0508203526193118,
-                "sdvLatency":0.5267024400632015,
-                "minLatency":0.326581,
-                "maxLatency":15.584396,
-                "failsCount":321,
-                "successCount":19738,
-                "failing":0
-            },
-            {
-                "hostname": "machine1",
-                "role": "follower",
-                "currentLatency":1.400807,
-                "averageLatency":1.0508203526193118,
-                "sdvLatency":0.5267024400632015,
-                "minLatency":0.326581,
-                "maxLatency":15.584396,
-                "failsCount":321,
-                "successCount":19738,
-                "failing":0
-            }
-        ]
-    }';*/
-
-    function randomData() {
-        var json = {"followers": []}
-        var num = Math.floor((Math.random()*20)+1);
-        var count = 0;
-        while (num > count) {
-            var follower = new Object();
-            follower.hostname = "etcd-host" + count;
-            follower.currentLatency = Math.floor((Math.random()*80)+1);
-
-            json.followers.push(follower);
-            count++;
-        }
-
-        return json;
-    }
-
-    $scope.machines = randomData();
-
-    function drawGraph () {
-        //hardcoded padding from chart json
-        var vert_padding = 30;
-        var hor_padding = 15;
-        //fetch width and height of graph area
-        var width = $($scope.graphContainer).width() - hor_padding;
-        var height = $($scope.graphContainer).height() - vert_padding;
-
-        // parse a spec and create a visualization view
-        function parse(spec) {
-            vg.parse.spec(spec, function(chart) {
-                chart({
-                    el: $scope.graphContainer,
-                    data: {
-                        "stats": $scope.machines.followers
-                    }
-                }).width(width).height(height).update();
-            });
-        }
-        parse("../etcd-latency.json");
-    }
-
-    $scope.show_table = function() {
-        $scope.tableVisibility = "etcd-table-reveal";
-    }
-
-    $scope.show_graph = function() {
-        $scope.tableVisibility = "etcd-table-hide";
-    }
-
-    $scope.getHeight = function() {
-        return $(window).height();
-    };
-    $scope.getWidth = function() {
-        return $(window).width();
-    };
-    $scope.$watch($scope.getHeight, function(newValue, oldValue) {
-        $(".etcd-body").css("height", $scope.getHeight()-5);
-        drawGraph();
-    });
-    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        drawGraph();
-    });
-    window.onresize = function(){
-        $scope.$apply();
-    }
-
+  $scope.getHeight = function() {
+    return $(window).height();
+  };
+  $scope.getWidth = function() {
+    return $(window).width();
+  };
+  $scope.$watch($scope.getHeight, function(newValue, oldValue) {
+    $(".etcd-body").css("height", $scope.getHeight()-5);
+    read();
+  });
+  $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+    read();
+  });
+  window.onresize = function(){
+    $scope.$apply();
+  }
+  // Update the graphs live
+  setInterval(function() {
+    read()
+    $scope.$apply();
+  }, 500);
 });
--- app/scripts/etcd.js
+++ app/scripts/etcd.js
@@ -5,7 +5,16 @@ angular.module('etcdResource', ['ngResource'])
 angular.module('etcdResource').factory("etcdKeys", function($resource, $http, $route, $location) {
   delete $http.defaults.headers.common['X-Requested-With'];
   return $resource('http://localhost:4001' + $location.path(), {
-  	path: "@path"
+    path: "@path"
+  }, {
+    get: {method: "GET"}
+  });
+});
+
+angular.module('etcdResource').factory("etcdStats", function($resource, $http, $route, $location) {
+  delete $http.defaults.headers.common['X-Requested-With'];
+  return $resource('http://localhost:4001/v1/stats/' + $location.path(), {
+    path: "@path"
   }, {
     get: {method: "GET"}
   });
