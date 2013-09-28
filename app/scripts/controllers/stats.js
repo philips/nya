8fca6e2028c8c82b95394f13c216c9d9e442d53d
Brandon Philips <brandon@ifup.co>
2013-09-27 17:12:13 -0700

    chore(etcd-latency): move into an angular modules

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('etcd-stats')
-  .controller('StatsCtrl', function ($scope, $http) {
+var etcdStats = angular.module('etcd-stats');
 
+etcdStats.controller('StatsCtrl', function ($scope, $http, statsVega) {
   $scope.graphContainer = '#latency';
   $scope.graphVisibility = 'etcd-graph-show';
   $scope.tableVisibility = 'etcd-table-hide';
@@ -41,7 +41,7 @@ angular.module('etcd-stats')
         }).width(width).height(height).update();
       });
     }
-    parse('../etcd-latency.json');
+    parse(statsVega);
   }
 
   $scope.showTable = function() {
@@ -75,3 +75,97 @@ angular.module('etcd-stats')
     $scope.$apply();
   }, 500);
 });
+
+/* statsVega returns the vega configuration for the stats dashboard */
+etcdStats.factory('statsVega', function () {
+  return {
+    'padding': {'top': 10, 'left': 5, 'bottom': 40, 'right': 10},
+    'data': [
+      {
+        'name': 'stats'
+      },
+      {
+        'name': 'thresholds',
+        'values': [50, 100]
+      }
+    ],
+    'scales': [
+      {
+        'name': 'y',
+        'type': 'ordinal',
+        'range': 'height',
+        'domain': {'data': 'stats', 'field': 'index'}
+      },
+      {
+        'name': 'x',
+        'range': 'width',
+        'domainMin': 0,
+        'domainMax': 100,
+        'nice': true,
+        'zero': true,
+        'domain': {'data': 'stats', 'field': 'data.latency.current'}
+      },
+      {
+        'name': 'color',
+        'type': 'linear',
+        'domain': [10, 50, 100, 1000000000],
+        'range': ['#00DB24', '#FFC000', '#c40022', '#c40022']
+      }
+    ],
+    'axes': [
+      {
+        'type': 'x',
+        'scale': 'x',
+        'ticks': 6,
+        'name': 'Latency (ms)'
+      },
+      {
+        'type': 'y',
+        'scale': 'y',
+        'properties': {
+          'ticks': {
+            'stroke': {'value': 'transparent'}
+          },
+          'majorTicks': {
+            'stroke': {'value': 'transparent'}
+          },
+          'labels': {
+            'fill': {'value': 'transparent'}
+          },
+          'axis': {
+            'stroke': {'value': '#333'},
+            'strokeWidth': {'value': 1}
+          }
+        }
+      }
+    ],
+    'marks': [
+      {
+        'type': 'rect',
+        'from': {'data': 'stats'},
+        'properties': {
+          'enter': {
+            'x': {'scale': 'x', 'value': 0},
+            'x2': {'scale': 'x', 'field': 'data.latency.current'},
+            'y': {'scale': 'y', 'field': 'index', 'offset': -1},
+            'height': {'value': 3},
+            'fill': {'scale':'color', 'field':'data.latency.current'}
+          }
+        }
+      },
+      {
+          'type': 'symbol',
+          'from': {'data': 'stats'},
+          'properties': {
+            'enter': {
+              'x': {'scale': 'x', 'field': 'data.latency.current'},
+              'y': {'scale': 'y', 'field': 'index'},
+              'size': {'value': 50},
+              'fill': {'value': '#000'}
+            }
+          }
+        }
+      ]
+    };
+});
+
